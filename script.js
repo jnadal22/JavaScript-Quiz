2915371c681c486ea6d9e3d3e1a180376e578281
Jordan Nadal <nadal_jordan@yahoo.com>
2022-07-12 18:18:01 -0500

    javaScript changes for questions to appear on page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // The variables are set to get the quiz started
 let highScoresEl = document.querySelector("#high-scores");
 let startBtnEl = document.querySelector("#start");
-let questionsEl = document.querySelector("#question-container");
+let questionsEl = document.querySelector("#question");
 let playerInitials = document.getElementById("intials");
 let TimerEl = document.querySelector("#Timer");
 let secondsEl = document.querySelector("#seconds");
@@ -27,7 +27,7 @@ const myQuestions = [
       c: "true/false",
       d: "100,000",
     },
-    correctAnswer: "c",
+    correctAnswer: "true/false",
   },
   {
     question: "An Array holds variables with: ",
@@ -36,7 +36,7 @@ const myQuestions = [
       b: "3 values",
       c: "multiple values",
     },
-    correctAnswer: "c",
+    correctAnswer: "multiple values",
   },
   {
     question: "JavaScript is and ___________ language ",
@@ -46,7 +46,7 @@ const myQuestions = [
       c: "procedural",
       d: "none of the above",
     },
-    correctAnswer: "a",
+    correctAnswer: "object-oriented",
   },
   {
     question: "which is used to define a variable?",
@@ -56,19 +56,51 @@ const myQuestions = [
       c: "a and b",
       d: "none of the above",
     },
-    correctAnswer: "c",
+    correctAnswer: "a and b",
   },
 ];
 
 // console.log(quizTimer)
-
+var idx = 0;
 let countdownTimer = 40;
 
+function checkAns(evt){
+evt.preventDefault()
+if (evt.target.textContent === myQuestions[idx].correctAnswer){
+  idx++
+  displayQuestions()
+}else{
+  countdownTimer -= 10
+  idx++
+  displayQuestions()
+}
+}
+
+function displayQuestions(){
+  
+  // for (var i = 0; i < 1; i++){
+    questionsEl.textContent = myQuestions[idx].question;
+    btn1El.textContent= myQuestions[idx].answers.a
+    btn2El.textContent= myQuestions[idx].answers.b
+    btn3El.textContent= myQuestions[idx].answers.c
+    btn4El.textContent= myQuestions[idx].answers.d
+    btn1El.addEventListener('click',checkAns)
+    btn2El.addEventListener('click',checkAns)
+    btn3El.addEventListener('click',checkAns)
+    btn4El.addEventListener('click',checkAns)
+
+
+
+  // }
+}
+
+
+
 function startCountdown() {
-  questionsEl.textContent = myQuestions[0].question;
+  displayQuestions()
   console.log(myQuestions[0]);
   introEl.style.display = "none";
-  questionsEl.style.display = "block";
+  // questionsEl.style.display = "block";
 
   const timerinterval = setInterval(function () {
     secondsEl.textContent = countdownTimer;
@@ -80,6 +112,8 @@ function startCountdown() {
     }
   }, 1000);
 }
+
+
 
 startBtnEl.addEventListener("click", startCountdown);
 
